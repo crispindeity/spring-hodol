1cd7e0522d8781c3c2df527a54230b1ba720ac52
geombong <shoy1415@gmail.com>
2022-06-22 23:13:39 +0900

    project init

---
+++ src/main/java/com/hodol/SpringHodolApplication.java
@@ -0,0 +1,13 @@
+package com.hodol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHodolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHodolApplication.class, args);
+	}
+
+}
