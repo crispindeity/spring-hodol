1c892206eb35e6f765018e9d0a7ddee27f51390c
HeechulShin <shoy1415@gmail.com>
2022-07-05 23:34:25 +0900

    불필요한 주석 제거

--- src/main/java/com/hodol/controller/PostController.java
+++ src/main/java/com/hodol/controller/PostController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 public class PostController {
-
-    //SSR -> jsp, thymeleaf, mustache, freemarker
-    //SPA ->
-    //  vue, nuxt
-    //  react, next
 
     private final PostService postService;
 
